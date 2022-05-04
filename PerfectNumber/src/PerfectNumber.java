5e70cef24b40a74f89c58bb8d0ed78d28b7fdf2e
Aslıhan Şimşek <simseekasli@gmail.com>
2022-05-04 18:28:08 +0300

    -Take a number from user -Find perfect number

--- PerfectNumber/src/PerfectNumber.java
+++ PerfectNumber/src/PerfectNumber.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class PerfectNumber {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please Enter a Number...:");
+        int number = scanner.nextInt();
+        int total = 0;
+        for (int i = 1; i < number;  i++) {
+            if (number % i == 0) {
+                total += i;
+            }
+        }
+
+        if (total == number){
+            System.out.println("Selected Number is Perfect Number");
+        }else {
+            System.out.println("Selected Number is  not Perfect Number");
+        }
     }
 }
